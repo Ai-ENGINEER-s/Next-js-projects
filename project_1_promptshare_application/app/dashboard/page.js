0b203493c85ca6178d52e0f4783bb2d551ec6fea
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-10-16 09:39:11 +0100

    next js link navigating through pages

---
+++ project_1_promptshare_application/app/dashboard/page.js
@@ -0,0 +1,16 @@
+
+
+const LayoutComponent = ()=>{
+
+
+    return (
+
+        <div>
+
+            <h1>This is a layout component and it has to be shared with all the componenent </h1>
+        </div>
+    )
+}
+
+
+export default LayoutComponent 
---
+++ project_1_promptshare_application/app/test/page.js
@@ -0,0 +1,20 @@
+
+import './test.css'  ; 
+const  TestComponent = ()=>{
+
+
+    return (
+        <div className='test'>
+
+<p>Acces test route </p>
+
+<h3>This is the content of the test directory </h3>
+        </div>
+
+    )
+
+
+}
+
+
+export default TestComponent 
